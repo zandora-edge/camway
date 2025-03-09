1f019aa9b7d942b3e4125d269cd2b7b5f683cd01
Mohammed Musthafa A <mohammed.musthafa.dev@gmail.com>
2025-03-09 14:59:08 +0300

    secondary commit

--- js/google-map.js
+++ js/google-map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var myLatlng = new google.maps.LatLng(10.7782621, 76.6285911);
     // 39.399872
     // -8.224454
     
